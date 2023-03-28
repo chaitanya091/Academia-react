637d19250a428abb71d2715e302bc97c55a62df8
chaitanya091 <chaitanyapatilcr7@gmail.com>
2023-03-28 15:35:04 +0530

    aboutcard created

--- src/components/about/AWrapper.jsx
+++ src/components/about/AWrapper.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { awrapper } from '../../dummydata'
 
 const AWrapper = () => {
   return (
     <>
-        <h1>AWrapper</h1>
+         <section className="awrapper">
+              <div className="container grid">
+                {awrapper.map((val)=>(
+                   <div className="box flex">
+                    <div className="img">
+                      <img src={val.cover} alt=""/>
+                    </div>
+                    <div className="text">
+                      <h1>{val.data}</h1>
+                      <h3>{val.title}</h3>
+                    </div>
+                   </div>
+                ))}
+              </div>
+         </section>
     </>
   )
 }
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import AboutCard from '../about/AboutCard';
 import Hero from './hero/Hero';
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
      <>
      
         <Hero/>
+        <AboutCard/>
          
      </>
   )
