c7802cefb3e4a1dc2226b23afee0406a32ecfe02
chaitanya091 <chaitanyapatilcr7@gmail.com>
2023-03-29 01:20:24 +0530

    teamcard component created

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Home from './components/home/Home'
 import About from './components/about/About'
 import CourseHome from './components/allcourses/CourseHome'
+import Team from './components/team/Team'
 const App  = () => {
   return (
     <>
@@ -14,6 +15,7 @@ const App  = () => {
           <Route path="/"  element={<Home/>} />
           <Route path="/about"  element={<About/>} />
           <Route path="/courses"  element={<CourseHome/>} />
+          <Route path="/team"  element={<Team/>} />
         </Routes>
       </Router>
     </>
---
+++ src/components/team/Team.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Back from '../common/back/Back';
+import TeamCard from './TeamCard';
+import './team.css'
+const Team = () => {
+  return (
+     <>
+     <Back title='Team'/>
+     <section className='team padding'>
+        <div className="container grid">
+            <TeamCard/>
+        </div>
+     </section>
+     </>
+  )
+}
+export default Team;
