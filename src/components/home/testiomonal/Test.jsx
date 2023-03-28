345bd7c5be8ce1048efa4ab039160d14b3af1180
chaitanya091 <chaitanyapatilcr7@gmail.com>
2023-03-29 02:12:11 +0530

    testimonal component added with style

--- src/components/home/testiomonal/Test.jsx
+++ src/components/home/testiomonal/Test.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import './style.css'
+import { testimonal } from '../../../dummydata'
+import Title from '../../common/title/Title'
 
 const Test = () => {
   return (
      <>
-        <h1>test</h1>
+         <section className="testimonal padding">
+            <div className="container">
+                <Title subtitle="testimonal" title='our Successful Students'/>
+                <div className='content grid2'>
+                    {testimonal.map((val)=>(
+                        <div className="items shadow">
+                             <div className="box flex">
+                                <div className="img">
+                                    <img src={val.cover} alt=""></img>
+                                    <i className='fa fa-quote-left icon'></i>
+                                </div>
+                                <div className="name">
+                                    <h2>{val.name}</h2>
+                                    <span>{val.post}</span>
+                                </div>
+                             </div>
+                             <p>{val.desc}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+         </section>
      </>
   )
 }
